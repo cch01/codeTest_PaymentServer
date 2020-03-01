ab080a0d4509303aefa8eeb5c243a91588447b29
cch01 <chanchiho01@gmail.com>
2020-03-01 21:07:32 +0800

    fixed middleWare naming

--- router.js
+++ router.js
@@ -2,12 +2,12 @@ import express from "express";
 import getUsers from "./apis/getUsers";
 import getTransactions from "./apis/getTransactions";
 import postTransaction from "./apis/postTransaction";
-import verifyTrasaction from "./utils/verifyTransaction";
+import verifyTransaction from "./utils/verifyTransaction";
 
 const router = express.Router();
 
 router.get("/api/get/users", getUsers);
 router.get("/api/get/transactions", getTransactions);
-router.post("/api/post/transaction", verifyTrasaction, postTransaction);
+router.post("/api/post/transaction", verifyTransaction, postTransaction);
 
 export default router;
